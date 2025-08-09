7a92b240643d5a75fb030beba49715ad013d676e
Deep-ai5 <deepchandram243@gmail.com>
2025-08-09 14:44:01 +0530

    Added initial Python assignment files

---
+++ Assignment 1/Task1.py
@@ -0,0 +1,10 @@
+x=int(input('Enter first number:'))
+y=int(input('Enter second number:'))
+add = x+y
+sub=x-y
+mul=x*y
+div=x/y
+print("Addition:",add)
+print("Subtraction:",sub)
+print("Multiplication:",mul)
+print("Division:",div)
---
+++ Assignment 1/Task2.py
@@ -0,0 +1,4 @@
+f=str(input("Enter First name"))
+l=str(input("Enter Last name"))
+print("Hello,",f+l,"! Welcome to Python program")
+
